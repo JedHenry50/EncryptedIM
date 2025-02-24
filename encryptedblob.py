ec40eb18c76f3b86f5dd16bfbd96b04c0c8aca1e
Jed Henry <156385037+JedHenry50@users.noreply.github.com>
2025-02-24 19:26:22 +0000

    Blob

--- encryptedblob.py
+++ encryptedblob.py
@@ -24,12 +24,24 @@ class EncryptedBlob:
         # TODO: MODIFY THE CODE BELOW TO ACTUALLY ENCRYPT 
         # AND GENERATE A SHA256-BASED HMAC BASED ON THE 
         # confkey AND authkey
+        iv = get_random_bytes(16)
+
 
         # pad the plaintext to make AES happy
-        plaintextPadded = pad(bytes(plaintext,'utf-8'),16) 
-        ciphertext = plaintextPadded  # definitely change this. :)
-        iv = bytes([0x00, 0x00, 0x00, 0x00])  # and this too!
-        mac = bytes([0x00, 0x00, 0x00, 0x00]) # and this too!
+        confkey_bytes = bytes(confkey, 'ascii')
+        authkey_bytes = bytes(authkey, 'ascii')
+
+        # Pad the plaintext
+        plaintext_padded = pad(plaintext.encode('utf-8'), AES.block_size)
+
+        # Encrypt using AES-CBC
+        cipher = AES.new(confkey_bytes, AES.MODE_CBC, iv)
+        ciphertext = cipher.encrypt(plaintext_padded)
+
+        # Compute HMAC using SHA-256 on (IV || Ciphertext)
+        hmac = HMAC.new(authkey_bytes, digestmod=SHA256)
+        hmac.update(iv + ciphertext)
+        mac = hmac.digest()
 
         # DON'T CHANGE THE BELOW.
         # What we're doing here is converting the iv, ciphertext,
@@ -49,19 +61,37 @@ class EncryptedBlob:
         # TODO: MODIFY THE CODE BELOW TO ACTUALLY DECRYPT
         # IF IT DOESN'T DECRYPT, YOU NEED TO RAISE A 
         # FailedDecryptionError EXCEPTION
+        confkey_bytes = bytes(confkey, 'ascii')
+        authkey_bytes = bytes(authkey, 'ascii')
 
+        hmac = HMAC.new(authkey_bytes, digestmod=SHA256)
+        hmac.update(iv + ciphertext)
+
+    
         # TODO: hint: in encryptThenMAC, I padded the plaintext.  You'll
         # need to unpad it.
         # See https://pycryptodome.readthedocs.io/en/v3.11.0/src/util/util.html#crypto-util-padding-module
 
         # so, this next line is definitely wrong.  :)
-        self.plaintext = "It's a wonderful day in the neighborhood."
         
         # TODO: DON'T FORGET TO VERIFY THE MAC!!!
         # IF IT DOESN'T VERIFY, YOU NEED TO RAISE A
         # FailedAuthenticationError EXCEPTION
+        try:
+            hmac.verify(received_mac)
+        except ValueError:
+            raise imexceptions.FailedAuthenticationError("MAC verification failed")
 
-        raise imexceptions.FailedAuthenticationError("ruh oh!")
+        # Decrypt using AES-CBC
+        cipher = AES.new(confkey_bytes, AES.MODE_CBC, iv)
+        plaintext_padded = cipher.decrypt(ciphertext)
+
+        # Remove padding
+        try:
+            plaintext = unpad(plaintext_padded, AES.block_size).decode('utf-8')
+        except ValueError:
+            raise imexceptions.FailedDecryptionError("Ruh oh")
+
         
 
         return self.plaintext
